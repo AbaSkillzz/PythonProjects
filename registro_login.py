5aaaa9508c7f0f8e571d01586804849d5764a97b
AbaSkillzz <75361009+AbaSkillzz@users.noreply.github.com>
2021-06-11 10:48:46 +0200

    Update registro_login.py

--- registro_login.py
+++ registro_login.py
@@ -7,16 +7,21 @@ import time
 PATH = "C:\Program Files (x86)\chromedriver"
 driver = webdriver.Chrome(PATH) #cio che gestisce le interazioni con l'web
 
+USER = str(input("inserisci lo username: "))
+PSW = str(input("inserisci la password: "))
+
+time.sleep(5)
+
 driver.get("https://aldini-valeriani-bo.registroelettronico.com/mastercom/")
 driver.implicitly_wait(5)
 
 utente = driver.find_element_by_name("user")
-utente.send_keys("529208")
+utente.send_keys(USER)
 utente.send_keys(Keys.RETURN)
 
 time.sleep(3)
 
 psw = driver.find_element_by_name("password_user")
-psw.send_keys("ababil2005")
+psw.send_keys(PSW)
 psw.send_keys(Keys.RETURN)
 
